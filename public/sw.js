4c3b1319318d0eb8ea4bf06a3d0332c08274314f
Alexander Drummond <113899680+alexanderdrummond@users.noreply.github.com>
2023-11-02 17:10:25 +0100

    service worker tweaked to cache for offline use

--- public/sw.js
+++ public/sw.js
@@ -1,18 +1,24 @@
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('api-cache').then((cache) => {
+      return cache.addAll([
+        '/Icon.svg',
+      ]);
+    })
+  );
+});
+
 self.addEventListener('fetch', (event) => {
-  if (event.request.url.includes('elprisenligenu.dk')) {
+  if (event.request.url.includes('elprisenligenu.dk') || event.request.url.endsWith('Icon.svg')) {
     event.respondWith(
       caches.match(event.request).then((cachedResponse) => {
         return cachedResponse || fetch(event.request).then((networkResponse) => {
-         
           const clonedResponseForJson = networkResponse.clone();
           const clonedResponseForCache = networkResponse.clone();
-          return clonedResponseForJson.json().then(data => {
-           
-            caches.open('api-cache').then((cache) => {
-              cache.put(event.request, clonedResponseForCache);
-            });
-            return networkResponse; 
+          caches.open('api-cache').then((cache) => {
+            cache.put(event.request, clonedResponseForCache);
           });
+          return clonedResponseForJson;
         });
       })
     );
