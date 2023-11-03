6e209e5789c2af91830318c51b1ed44299d3ee2c
Alexander Drummond <113899680+alexanderdrummond@users.noreply.github.com>
2023-11-03 22:12:01 +0100

    fallback addition

--- public/sw.js
+++ public/sw.js
@@ -1,3 +1,11 @@
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('offline-cache').then((cache) => {
+      return cache.add('/offline.html');
+    })
+  );
+});
+
 self.addEventListener('fetch', (event) => {
   if (event.request.url.includes('elprisenligenu.dk')) {
     event.respondWith(
@@ -17,7 +25,11 @@ self.addEventListener('fetch', (event) => {
   } else {
     event.respondWith(
       caches.match(event.request).then((response) => {
-        return response || fetch(event.request);
+        return response || fetch(event.request).catch(() => {
+          return caches.open('offline-cache').then((cache) => {
+            return cache.match('/offline.html');
+          });
+        });
       })
     );
   }
